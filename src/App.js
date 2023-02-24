42faa5ba84f932623c62116176fd0786dbda49f3
fariscoric <faris.coric@gmail.com>
2023-02-24 15:51:18 +0100

    Commit test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          QUIZZICAL APPLICATION
         </a>
       </header>
     </div>
