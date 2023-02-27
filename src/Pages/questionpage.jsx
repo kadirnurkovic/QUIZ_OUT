39b35dab20a99c47807c6331a3690560e4bfa824
kadirnurkovic <kadirnurkovic0404@gmail.com>
2023-02-27 13:48:50 +0100

    +

--- src/Pages/questionpage.jsx
+++ src/Pages/questionpage.jsx
@@ -7,12 +7,10 @@ function QuestionPage() {
   const [answers, setAnswers] = useState([]);
   const getApi = () => {
     axios.get("https://the-trivia-api.com/api/questions").then((response) => {
-      setQuestions(response.data);
+      setQuestions(response.data.slice(0,1));
     });
   };
   
-
-
   useEffect(() => {
     getApi();
   }, []);
@@ -24,7 +22,7 @@ function QuestionPage() {
           <h1>{el.question}</h1>
           <div>
             <div>{el.incorrectAnswers.concat(el.correctAnswer).sort((a,b)=> Math.round(0 - Math.random())).map((element, id) => (
-              <div key={id}>{element === el.correctAnswer ? "True" : "Not"}</div>
+              <div key={id} style={{color: element === el.correctAnswer ? "red" : "black"}}>{element}</div>
             ))}
 </div>
           </div>
