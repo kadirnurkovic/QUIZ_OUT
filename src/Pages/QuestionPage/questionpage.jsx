68051a80f2b458a30ae113d433847dc6b6fe2317
kadirnurkovic <kadirnurkovic0404@gmail.com>
2023-03-02 23:03:30 +0100

    ch

--- src/Pages/QuestionPage/questionpage.jsx
+++ src/Pages/QuestionPage/questionpage.jsx
@@ -1,54 +1,76 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import "./questionpage.css"
-import { useLocation, useNavigate } from 'react-router-dom'
+import "./questionpage.css";
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function QuestionPage() {
-  const [sliceFirst, setSliceFirst] = useState(0)
-  const [sliceSecond, setSliceSecond] = useState(1)
-  const [questionCounter, setQuestionCounter] = useState(1)
 
-  const navigate = useNavigate()
+  const [questionCounter, setQuestionCounter] = useState(1);
+  const [nextQuestion, setNextQuestion] = useState(0);
+  const [activeClass, setActiveClass] = useState(false);
+  
+  const navigate = useNavigate();
 
-  const { state } = useLocation();
-  console.log(state.quest)
+  const handleNextQuestion = () => {
+    const nextQuestionIncrementer = nextQuestion + 1;
+    setNextQuestion(nextQuestionIncrementer);
+    setQuestionCounter(questionCounter+1);
 
-  const onClickHandler = () => {
-    setSliceFirst(sliceFirst+1)
-    setSliceSecond(sliceSecond+1)
-    setQuestionCounter(questionCounter+1)
     if (questionCounter >= state.quest.length) {
       navigate("/summary")
     }
   }
 
+  const { state } = useLocation();
+  console.log(state.quest);
+
+  const toggleClassHandler = () => {
+    if(activeClass){
+      return 'active'
+    }else if(!activeClass){
+      return 'four-answers'
+    }
+  };
+  
+  console.log(state.quest[0])
+
+  const handleActiveClass = () => {
+    setActiveClass(true);
+  };
+
   return (
     <div className="main-div">
-      {state.quest?.slice(sliceFirst, sliceSecond).map((el) => (
-        <div key={el.id}
+        <div
         className="container-container">
-          <h1 className="question-div">{el.question}</h1>
+          <h1 className="question-div">{state.quest[nextQuestion].question}</h1>
           <div className="line"></div>
             <div className="answers-container">
-              {el.incorrectAnswers
-                .concat(el.correctAnswer)
-                .sort((a, b) => Math.round(0 - Math.random()))
-                .map((element, id) => (
-                  <div
-                    className="four-answers"
-                    key={id}
-                    style={{
-                      color: element === el.correctAnswer ? "red" : "black",
-                    }}
-                    onClick={() => onClickHandler()}
-                  >
-                    {element}
-                  </div>
-                ))}
+              {state.quest.map((el) => (
+                <div key={el.id}>{el === state.quest[nextQuestion] ? (el.incorrectAnswers.concat(el.correctAnswer)
+                  .sort(() => Math.round(0 - Math.random()))
+                  .map((element, id) => (
+                    <div
+                      key={id}
+                      className={toggleClassHandler()}
+                      style={{
+                        color: element === el.correctAnswer ? "red" : "black",
+                      }}
+                      onClick={() => {
+                        handleNextQuestion()
+                        if(el.correctAnswer === element){
+                        handleActiveClass(id)
+                        }
+                      }}
+                    >
+                      {console.log(el)}
+                      {element}
+                    </div>
+              ))) : ''}</div>))}
+                
           </div>
         </div>
-      ))}
+      
     </div>
   );
 }
