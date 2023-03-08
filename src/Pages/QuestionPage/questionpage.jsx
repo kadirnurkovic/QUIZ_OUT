0d5a6d8c2e05a43f9a25a1aead069300891e7d6d
kadirnurkovic <kadirnurkovic0404@gmail.com>
2023-03-08 17:12:04 +0100

    timer

--- src/Pages/QuestionPage/questionpage.jsx
+++ src/Pages/QuestionPage/questionpage.jsx
@@ -7,7 +7,8 @@ import MainPage from "../MainPage/MainPage";
 import { ApiContext } from '../../context/context';
 
 const QuestionPage = () => {
-  const { shuffled } = useContext(ApiContext);
+  
+  const [timer, setTimer] = useState(60);
   const [newData, setNewData] = useState(
     JSON.parse(localStorage.getItem("data"))
   );
@@ -19,24 +20,48 @@ const QuestionPage = () => {
   );
   const [isActive, setIsActive] = useState(false);
   const navigate = useNavigate();
+
   const handleNextQuestion = () => {
+    setIsActive(true)
     const nextQuestionIncrementer = currentQuestion + 1;
     setCurrentQuestion(nextQuestionIncrementer);
     setQuestionCounter(questionCounter + 1);
-    setIsActive(true)
+    setIsActive(false)
     if (+localStorage.getItem("incrementer") >= newData.length) {
       navigate("/summary");
       setQuestionCounter(questionCounter + 1);
     }
+  
   };
+
+  console.log(isActive)
+  // Timer function
+    setTimeout(() => {
+      setTimer(timer - 1)
+    }, 1000);
+
+    if(timer === 0){
+      navigate('/summary')
+    }
+
   localStorage.setItem("slice", currentQuestion);
   localStorage.setItem("incrementer", questionCounter);
 
   let answers = JSON.parse(localStorage.getItem('answers'));
+ 
+  useEffect(() => {
+     if(newData.length === 10){
+      setTimer(30);
+     }else if(newData.length === 30){
+      setTimer(90)
+     }
+  },[])
 
   return (
     <div className="main-div">
       <div className="container-container">
+        <div style={{color: 'white'}}>{timer}</div>
+        <h2 className="question-counter">{localStorage.getItem("incrementer")}/{newData.length}</h2>
         <h1 className="question-div">
           {newData[localStorage.getItem("slice")].question}
         </h1>
@@ -45,15 +70,14 @@ const QuestionPage = () => {
           {answers[+(localStorage.getItem('slice'))].map((element, id) => (
             <div
               key={id}
-              className={isActive ? "four-answersCorrect" : "four-answers"}
+              className="four-answers"
               style={
-                element ===
-                  newData[+localStorage.getItem("slice")].correctAnswer &&
-                isActive
-                  ? { boxShadow: "0 0 10px 3px rgb(21, 255, 0)" }
-                  : { boxShadow: "red" }
+                isActive && element ===
+                  newData[+localStorage.getItem("slice")].correctAnswer ?
+                   { boxShadow: "0 0 10px 3px rgb(21, 255, 0)" } : !isActive ? {} : {boxShadow: "0 0 10px 3px rgb(189, 11, 11)"}
               }
               onClick={() => {
+                clearTimeout();
                 setIsActive(true)
                 setTimeout(() => {
                   handleNextQuestion();
